24bbe25b30bf74e0609b1b8d4adfe49adfc82aa0
Danny Mas <dmas@lsit-inc.com>
2024-09-16 16:20:23 -0400

    changed react setting

--- frontend/container/src/App.tsx
+++ frontend/container/src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import MainDashboard from './components/MainDashboard';
 
-function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <MainDashboard />
-      </div>
-    </Router>
-  );
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navigation from './components/Navigation';
+import Header from './components/Header.tsx';
+import Footer from './components/Footer';
+import MainDashboard from './components/MainDashboard';
+import SportsDashboard from './components/Sports/SportsDashboard';
+import FinanceDashboard from './components/Finance/FinanceDashboard';
+import WeatherTravelDashboard from './components/WeatherTravel/WeatherTravelDashboard';
+import NewsDashboard from './components/News/NewsDashboard';
+
+const App: React.FC = () => (
+  <Router>
+    <div className="app">
+      <Navigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<MainDashboard />} />
+          <Route path="/sports/*" element={<SportsDashboard />} />
+          <Route path="/finance/*" element={<FinanceDashboard />} />
+          <Route path="/weather-travel/*" element={<WeatherTravelDashboard />} />
+          <Route path="/news/*" element={<NewsDashboard />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
